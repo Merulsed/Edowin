57445509c360140d34aa5f0e9fdd59eeeaf47a30
Yang Enrique Silva Neri <yang.silva.neri@gmail.com>
2013-11-21 08:20:40 -0600

    La base

---
+++ Base/bd.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE eduwin;
+USE eduwin;
+
+CREATE TABLE Usuario (
+	ID INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(30) NOT NULL,
+	password VARCHAR(60) NOT NULL,
+	mail VARCHAR(45) NOT NULL,
+	nombre VARCHAR(45) NOT NULL,
+	apellidoP VARCHAR(45) NOT NULL,
+	apellidoM VARCHAR(45) NOT NULL,
+	esAdmin BOOLEAN NOT NULL,
+	ultimoAcceso DATETIME,
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE Archivo (
+	idArchivo INT NOT NULL AUTO_INCREMENT,
+	nombre VARCHAR(45) NOT NULL,
+	tipo VARCHAR(45) NOT NULL,
+	numPaginas INT,
+	url VARCHAR(60),
+	numDescargas INT,
+	Publico BOOLEAN NOT NULL,
+	fechaCreacion DATETIME NOT NULL,
+	userID INT NOT NULL,
+	PRIMARY KEY(idArchivo),
+	FOREIGN KEY (userID)
+    REFERENCES Usuario(ID)
+);
+
+INSERT INTO Usuario (username, password, mail, nombre, esAdmin) VALUES ('ChuckNorris', 'clasico', 'yang.silva.neri@gmail.com', 'Chuck Norris', true);
