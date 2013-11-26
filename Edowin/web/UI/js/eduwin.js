f08490d54800dc9fbdf2d538976221d8ee4ff784
Julio Montaño <jmzvetica@gmail.com>
2013-11-25 20:29:56 -0600

    UI

--- Edowin/web/UI/js/eduwin.js
+++ Edowin/web/UI/js/eduwin.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	
 	// $('.sidebar').css('left', '-270px');
+	var formWidth = 1;
+	$('.not-sidebar form').css('width', ($('.not-sidebar form .inputbox').width()+34)*formWidth);
+	$('form width1').css('width', ($('.not-sidebar form .inputbox').width())*formWidth);
 
 });
